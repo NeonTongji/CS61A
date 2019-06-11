1a87049dc0bea73e4ace5fdcc49b94f94de082c4
j19950805 <j19950805@gmail.com>
2019-06-11 10:20:27 -0700

    Q1~Q7 finished

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_nineteen():
+    """Come up with the most creative expression that evaluates to 2019,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_nineteen()
+    2019
+    """
+    return 19950805 - 19930105 + 1314 - 19995
