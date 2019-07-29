44ce389830a89a4723d508939108cd046fa4961f
j19950805 <j19950805@gmail.com>
2019-07-29 10:45:59 +0800

    mario_number modified.

--- disc/guerrilla02.py
+++ disc/guerrilla02.py
@@ -1,15 +1,21 @@
 
 def mario_number(level):
-    def double(level, num):
-        sum_num = 0
-        while level:
-            if level % 100 == num:
-                sum_num, level = sum_num + 1, level // 100
-            else:
-                level = level // 10
-        return sum_num
-    if double(level, 0) != 0:
+    """
+    Return the number of ways that Mario can traverse the level,
+    where Mario can either hop by one digit or two digits each turn.
+    A level is defined as being an integer with digits where a 1 is
+    something Mario can step on and 0 is something Mario cannot step
+    on.
+    >>> mario_number(10101) # Hops each turn: (1, 2, 2)
+    1
+    >>> mario_number(11101) # Hops each turn: (1, 1, 1, 2), (2, 1, 2)
+    2
+    >>> mario_number(100101)# No way to traverse through level
+    0
+    """
+    if level == 1:
+        return 1
+    elif level % 10 == 0:
         return 0
     else:
-        return double(level, 11) + 1 
-
+        return mario_number(level // 10) + mario_number(level // 100)
